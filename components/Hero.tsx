f4d8d7cd8734ae937995217e7bbc2caf858d9590
ankitku3101 <ankit.ku3101@gmail.com>
2025-04-04 22:24:13 +0530

    Added: Next Auth

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion"; // Corrected import
+import React from "react";
+import { motion } from "motion/react";
 import Image from "next/image";
 import Navbar from "./ui/Navbar";
 import BackgroundGradient from "./BackgroundGradient";
@@ -10,13 +10,6 @@ import { FlipWords } from "./ui/flip-words";
 const words = ["innovation", "collaboration", "growth", "success"];
 
 function Hero() {
-  // Prevent hydration mismatch
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <section
       id="hero"
@@ -28,27 +21,21 @@ function Hero() {
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-3xl font-bold text-[#27187E] md:text-5xl lg:text-7xl">
           {"Transforming Learning Empowering Colleges"
             .split(" ")
-            .map((word, index) =>
-              isMounted ? ( // Prevent hydration mismatch
-                <motion.span
-                  key={index}
-                  initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
-                  animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-                  transition={{
-                    duration: 0.3,
-                    delay: index * 0.1,
-                    ease: "easeInOut",
-                  }}
-                  className="mr-2 inline-block"
-                >
-                  {word}
-                </motion.span>
-              ) : (
-                <span key={index} className="mr-2 inline-block">
-                  {word}
-                </span>
-              )
-            )}
+            .map((word, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+                transition={{
+                  duration: 0.3,
+                  delay: index * 0.1,
+                  ease: "easeInOut",
+                }}
+                className="mr-2 inline-block"
+              >
+                {word}
+              </motion.span>
+            ))}
         </h1>
 
         {/* Subtext with FlipWords */}
@@ -59,7 +46,7 @@ function Hero() {
           className="relative z-10 mx-auto py-4 text-center text-lg font-medium text-[#27187E]"
         >
           Enhancing learning, connecting minds, and streamlining{" "}
-          {isMounted && <FlipWords words={words} />}
+          <FlipWords words={words}  />
         </motion.p>
 
         {/* CTA Buttons */}
@@ -91,7 +78,6 @@ function Hero() {
               className="aspect-[16/9] h-auto w-full object-cover"
               height={1000}
               width={1000}
-              priority 
             />
           </div>
         </motion.div>
---
+++ lib/authOptions.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const facultySchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            trim:true,
+            required:[true,"Name is mandatory."],
+        },
+        username:{
+            type:String,
+            required:[true,"Username is mandatory."],
+            trim:true,
+            lowercase:true,
+            unique:true
+        },
+        email:{
+            type:String,
+            required:[true,"Email is mandatory."],
+            lowercase:true,
+            trim:true,
+        },
+        password:{
+            type:String,
+            required:[true,"Password is mandatory."],
+        },
+        createdCourses:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"course"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.models.faculty || mongoose.model("faculty",facultySchema);
