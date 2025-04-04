8e365541a2afb95fbdfebb042558535f8c61143d
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-04 16:05:10 +0530

    Student Schema defined.

---
+++ models/student.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            trim:true,
+            required:[true,"Name is mandatory."],
+        },
+        username:{
+            type:String,
+            required:[true,"Username is mandatory."],
+            trim:true,
+            lowercase:true,
+            unique:true
+        },
+        email:{
+            type:String,
+            required:[true,"Email is mandatory."],
+            lowercase:true,
+            trim:true,
+        },
+        password:{
+            type:String,
+            required:[true,"Password is mandatory."],
+        },
+        enrolledincourse:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"course"
+            }
+        ]
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.models.student || mongoose.model("student",studentSchema);
