3d896a80b2b93f83ecd988b6dcff53b31c941646
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-05 05:39:08 +0530

    Quiz Creation and Question adding to the Quiz defined.

---
+++ app/api/v1/quiz/create-quiz/[id]/route.ts
@@ -0,0 +1,38 @@
+import quiz from "@/models/quiz";
+import dbConnect from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+interface Param{
+    params:{id:string}
+}
+
+export async function POST(req:NextRequest,{params}:Param){
+    try {
+        await dbConnect();
+        
+        const {id} = await params;
+        console.log("Here")
+        const reqBody = await req.json();
+        const {isactive=false,lastdate} = reqBody;
+        
+
+        if(!id || !mongoose.Types.ObjectId.isValid(id)){
+            return NextResponse.json({message:"Invalid id"},{status:400});
+        }
+
+        const createQuiz = await quiz.create({
+            belongsto:new mongoose.Types.ObjectId(id),
+            isactive,
+            lastdate
+        })
+
+        if(!createQuiz){
+            return NextResponse.json({message:"Quiz creatoin failed"},{status:400});
+        }
+
+        return NextResponse.json({message:"Created quiz",data:createQuiz},{status:200});
+    } catch (error:any) {
+        return NextResponse.json({message:error.message||"Some error happened",error},{status:500});
+    }
+}
